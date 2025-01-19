b5d60b954efd1c16da540c6f7218f65d4524259b
Privatech <ziga.slatnar.stagar@gmail.com>
2025-01-19 11:49:52 +0100

    Izziv 5

--- src/Izziv5.java
+++ src/Izziv5.java
@@ -1,9 +1,40 @@
 import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
 
 public class Izziv5 {
-    protected static class Oseba implements Comparable<Oseba> {
 
-        public static int attr = 0;
+    private static final String[] priimki = new String[]{"Novak", "Horvat", "Kovačič", "Krajnc", "Zupančič", "Kovač", "Potočnik", "Mlakar", "Vidmar", "Kos"};
+    private static final String[] imena = new String[]{"Franc", "Janez", "Marko", "Andrej", "Luka", "Ana", "Maja", "Mojca", "Nina", "Eva"};
+
+    public static void main(String[] args) {
+        // Randomly create
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Vnesi število oseb:");
+        final int amount = scanner.nextInt();
+        Oseba[] osebe = new Oseba[amount];
+        for (int i = 0; i < amount; i++) {
+            osebe[i] = new Oseba(priimki[new Random().nextInt(priimki.length)], imena[new Random().nextInt(imena.length)],
+                    new Random().nextInt(80));
+        }
+        do {
+            Oseba[] osebeCopy = Arrays.copyOf(osebe, osebe.length);
+            System.out.println("Seznam:");
+            for (int i = 0; i < amount; i++) {
+                System.out.println(osebeCopy[i].toString());
+            }
+            System.out.println("Vnesi atribut po katerem atributu sortirati (0-2):");
+            Oseba.attr = scanner.nextInt();
+            System.out.println("Vnesi smer urejanja (ASC ali DESC):");
+            SmerUrejanja smerUrejanja = SmerUrejanja.valueOf(scanner.next());
+            bubbleSort(osebeCopy, smerUrejanja);
+            System.out.println("Želiš ponoviti? (true ali false)");
+        } while (scanner.nextBoolean());
+    }
+
+    public static class Oseba implements Comparable<Oseba> {
+
+        public static int attr = -1;
 
         private String priimek;
         private String ime;
@@ -62,24 +93,23 @@ public class Izziv5 {
                 case 2:
                     return "" + letoR;
                 default:
-                    return "";
+                    return "%s %s (%d let)".formatted(priimek, ime, letoR);
             }
         }
     }
 
-    enum SmerUrejanja {
+    public static enum SmerUrejanja {
         ASC,
         DESC
     }
     
-    public void bubbleSort(Oseba[] elements, SmerUrejanja smerUrejanja) {
-        System.out.println(Arrays.toString(elements));
+    public static void bubbleSort(Oseba[] elements, SmerUrejanja smerUrejanja) {
         int i = 0; // Also counts as last swapped index
         while (i < elements.length - 1) {
             boolean swapped = false;
             int lastSwappedIndex = elements.length - 1;
             for (int j = elements.length - 1; j > i; j--) {
-                if (smerUrejanja == SmerUrejanja.ASC && elements[j - 1].compareTo(elements[j]) < 0) {
+                if (smerUrejanja == SmerUrejanja.ASC && elements[j - 1].compareTo(elements[j]) > 0) {
                     final Oseba temp = elements[j - 1];
                     elements[j - 1] = elements[j];
                     elements[j] = temp;
@@ -95,6 +125,7 @@ public class Izziv5 {
             for (int j = i; j < elements.length; j++) {
                 System.out.print(" " + elements[j].toString());
             }
+            System.out.println();
             if (!swapped) {
                 break;
             }
